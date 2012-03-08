432405454e3e34883d43329f2ef4d7a716b5d9aa
Kristof Van Laerhoven <kryztov@gmail.com>
2012-03-08 13:29:31 +0100

    removed object files

--- firmware/HedgeHog/HHOS.X/build/mv_sampling_oled/production/_ext/1360904562/rtcc.o.d
+++
@@ -1,7 +0,0 @@
-build/mv_sampling_oled/production/_ext/1360904562/rtcc.o : \
-	/home/essuser/HedgeHog/firmware/HedgeHog/RTC/rtcc.c \
-	/home/essuser/HedgeHog/firmware/HedgeHog/RTC/rtcc.h \
-	/opt/microchip/mplabc18/v3.40/h/p18f46j50.h \
-	/opt/microchip/Microchip/Include/GenericTypeDefs.h \
-	/opt/microchip/mplabc18/v3.40/h/stddef.h \
-
--- firmware/HedgeHog/HHOS.X/build/mv_sampling_oled/production/_ext/1472/osc.o.d
+++
@@ -1,5 +0,0 @@
-build/mv_sampling_oled/production/_ext/1472/osc.o : \
-	/home/essuser/HedgeHog/firmware/HedgeHog/osc.c \
-	/home/essuser/HedgeHog/firmware/HedgeHog/osc.h \
-	/opt/microchip/mplabc18/v3.40/h/p18f46j50.h \
-
