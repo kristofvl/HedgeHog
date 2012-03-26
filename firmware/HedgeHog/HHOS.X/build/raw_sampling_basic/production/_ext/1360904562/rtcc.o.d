a7ed2f06501f6974d5403c8d389afb22f6047400
Kristof Van Laerhoven <kryztov@gmail.com>
2012-03-26 10:46:52 +0200

    Revert "removed Taimoor's binaries"
    This reverts commit 4ab54760302c39e0dc34f024398ab60fbad1b990.

---
+++ firmware/HedgeHog/HHOS.X/build/raw_sampling_basic/production/_ext/1360904562/rtcc.o.d
@@ -0,0 +1,7 @@
+build/raw_sampling_basic/production/_ext/1360904562/rtcc.o : \
+	/home/taimoor/Desktop/Gits/HedgeHog/firmware/HedgeHog/RTC/rtcc.c \
+	/home/taimoor/Desktop/Gits/HedgeHog/firmware/HedgeHog/RTC/rtcc.h \
+	/opt/microchip/mplabc18/v3.40/h/p18f46j50.h \
+	/opt/microchip/Microchip/Include/GenericTypeDefs.h \
+	/opt/microchip/mplabc18/v3.40/h/stddef.h \
+
---
+++ firmware/HedgeHog/HHOS.X/build/raw_sampling_basic/production/_ext/224044192/usb_function_cdc.o.d
@@ -0,0 +1,23 @@
+build/raw_sampling_basic/production/_ext/224044192/usb_function_cdc.o : \
+	/opt/microchip/Microchip/USB/CDC\ Device\ Driver/usb_function_cdc.c \
+	/opt/microchip/mplabc18/v3.40/h/stdio.h \
+	/opt/microchip/mplabc18/v3.40/h/stdlib.h \
+	/opt/microchip/mplabc18/v3.40/h/string.h \
+	/opt/microchip/Microchip/Include/USB/usb_common.h \
+	/opt/microchip/Microchip/Include/USB/usb_device.h \
+	/opt/microchip/Microchip/Include/USB/usb_hal.h \
+	/opt/microchip/Microchip/Include/USB/usb_function_cdc.h \
+	/opt/microchip/Microchip/Include/USB/usb.h \
+	/opt/microchip/Microchip/Include/GenericTypeDefs.h \
+	/opt/microchip/mplabc18/v3.40/h/stddef.h \
+	/opt/microchip/Microchip/Include/Compiler.h \
+	/opt/microchip/mplabc18/v3.40/h/p18cxxx.h \
+	/opt/microchip/mplabc18/v3.40/h/p18f46j50.h \
+	/opt/microchip/mplabc18/v3.40/h/stdarg.h \
+	/home/taimoor/Desktop/Gits/HedgeHog/firmware/HedgeHog/usb_config.h \
+	/opt/microchip/mplabc18/v3.40/h/limits.h \
+	/opt/microchip/Microchip/Include/USB/usb_ch9.h \
+	/opt/microchip/Microchip/Include/USB/usb_hal_pic18.h \
+	/home/taimoor/Desktop/Gits/HedgeHog/firmware/HedgeHog/HardwareProfile.h \
+	/home/taimoor/Desktop/Gits/HedgeHog/firmware/HedgeHog/HardwareProfileBasic.h \
+
