7233fe32606130616a8a2864c48ebea6fb0042c8
Kristof Van Laerhoven <kryztov@gmail.com>
2013-10-10 14:00:37 +0200

    removed some binaries

--- firmware/HedgeHog/HHOS.X/build/raw_sampling_basic/production/_ext/677419432/ambient_light.o.d
+++
@@ -1,8 +0,0 @@
-build/raw_sampling_basic/production/_ext/677419432/ambient_light.o:  \
-	/home/hany/HedgeHog/firmware/HedgeHog/ambient_light\ driver/ambient_light.c  \
-	/home/hany/HedgeHog/firmware/HedgeHog/ambient_light\ driver/ambient_light.h  \
-	/opt/microchip/microchip_solutions_v2012-02-15/Microchip/Include/Compiler.h  \
-	/opt/microchip/microchip_solutions_v2012-02-15/Microchip/Include/GenericTypeDefs.h  \
-	/home/hany/HedgeHog/firmware/HedgeHog/HardwareProfile.h  \
-	/home/hany/HedgeHog/firmware/HedgeHog/HardwareProfileBasic.h  \
-
