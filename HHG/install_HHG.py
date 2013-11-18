f3e0af9253b0282d2d9f9e91a2e7e91bd019e509
Hany Abdulsamad <hany.abdulsamad@gmail.com>
2013-11-18 11:13:31 +0100

    small changes in hardware

--- HHG/install_HHG.py
+++ HHG/install_HHG.py
@@ -8,5 +8,7 @@ home_dir = os.environ['HOME']
 curr_dir = os.getcwd()
 
 subprocess.call(["mkdir", "-p", home_dir+"/.hhg"])
-subprocess.call(["cp", "conf_HHG.py", "Conf.ui", "start_HHG.py", "Start.ui", "plot_HHG.py", "HHG", home_dir+"/.hhg"])
-subprocess.call(["cp", "-r", "hhg_dialogs", "hhg_io", "hhg_features", "hhg_plot", home_dir+"/.hhg"])
+
+subprocess.call(["cp", "conf_HHG.py", "Conf.ui", "start_HHG.py", "Start.ui", "plot_HHG.py","dd_HHG.py", home_dir+"/.hhg"])
+
+subprocess.call(["cp", "-r", "hhg_dialogs", "hhg_io", "hhg_features", "hhg_plot", "dd_img", home_dir+"/.hhg"])
