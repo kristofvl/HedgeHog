9b8aad3a7e96ef454faa5d0c96377822ff027596
Hany Abdulsamad <hany.abdulsamad@gmail.com>
2013-12-10 15:28:55 +0100

    removed relative linking

--- HHG/install_HHG.py
+++ HHG/install_HHG.py
@@ -8,7 +8,5 @@ homeDir = os.environ['HOME']
 currDir = os.getcwd()
 
 subprocess.call(["mkdir", "-p", homeDir+"/.hhg"])
-
-subprocess.call(["ln", "-rs", "conf_HHG.py", "Conf.ui", "start_HHG.py", "Start.ui", "plot_HHG.py","dd_HHG.py", homeDir+"/.hhg"])
-
-subprocess.call(["ln", "-rs", "hhg_dialogs", "hhg_io", "hhg_features", "hhg_plot", "dd_img", homeDir+"/.hhg"])
+subprocess.call(["ln", "-s", "conf_HHG.py", "Conf.ui", "start_HHG.py", "Start.ui", "plot_HHG.py","dd_HHG.py", homeDir+"/.hhg"])
+subprocess.call(["ln", "-s", "hhg_dialogs", "hhg_io", "hhg_features", "hhg_plot", "dd_img", homeDir+"/.hhg"])
