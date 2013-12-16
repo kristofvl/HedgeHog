528bc066e6ac6f829ca6f7cd7c61ca3e7e0dd4d4
Eugen Berlin <eugen.berlin@gmail.com>
2013-12-16 11:04:26 +0100

    renaming hardware profiles according to actual eagleCAD iterations

--- firmware/HedgeHog/HardwareProfileOLED513.h
+++ firmware/HedgeHog/HardwareProfileOLED513.h
@@ -1,14 +1,14 @@
 /********************************************************************
- FileName:     	HardwareProfileBasicNew.h
+ FileName:     	HardwareProfileOLED.h
  Dependencies:
  Processor:	PIC18F46J50
- Hardware:	Porcupine HedgeHog BASIC
+ Hardware:	Porcupine HedgeHog OLED
  Compiler:  	Microchip C18
- Author:        EBerlin
+ Author:        KristofVL
  ********************************************************************/
 
-#ifndef HARDWARE_PROFILE_HEDGEHOG_BASIC_418_H
-#define HARDWARE_PROFILE_HEDGEHOG_BASIC_418_H
+#ifndef HARDWARE_PROFILE_HEDGEHOG_OLED_513_H
+#define HARDWARE_PROFILE_HEDGEHOG_OLED_513_H
 
 /*******************************************************************/
 /******** Board-specific definitions *******************************/
@@ -22,7 +22,7 @@ extern char is_logging;
 #define GetInstructionClock() ((is_logging)?CLKFREQ_LOG:CLKFREQ_USB)
 #define GetSystemClock() (CLKFREQ_USB)
 
-//#define DISPLAY_ENABLED		// don't use the oled display
+#define DISPLAY_ENABLED			// use the oled display
 //#define SOFTSTART_ENABLED		// don't use the softstart circuit
 #define ADXL345_ENABLED			// use the ADXL345 accelerometer
 //#define BMA150_ENABLED		// don't use BMA150 accelerometer
@@ -37,6 +37,13 @@ extern char is_logging;
 //#define USE_USB_BUS_SENSE_IO
 #define tris_usb_bus_sense	TRISCbits.TRISC2    // Input
 #define USB_BUS_SENSE		1
+
+/*******************************************************************/
+/******** Bootloader-compatible HEX file ***************************/
+/*******************************************************************/
+//Uncomment this to make the output HEX of this project 
+//   to be able to be bootloaded using the HID bootloader
+//#define PROGRAMMABLE_WITH_USB_HID_BOOTLOADER		
 
 /*******************************************************************/
 /******** SPI interface definitions ********************************/
@@ -62,47 +69,68 @@ extern char is_logging;
 /*******************************************************************/
 /******** MDD File System selection options ************************/
 /*******************************************************************/
-#define USE_PIC18                               // used by FSConfig.h
+#define USE_PIC18			// used by FSConfig.h
 #define USE_SD_INTERFACE_WITH_SPI
-#define SD_CS				PORTBbits.RB5       // Chip Select
-#define SD_CS_TRIS			TRISBbits.TRISB5
-#define SD_CD				0                   // Card Detect
-#define SD_CD_TRIS			TRISAbits.TRISA3
-#define SD_WE				0
+#define TRIS_CARD_DETECT	TRISBbits.TRISB4    // Input
+#define CARD_DETECT			PORTBbits.RB4
+#define TRIS_WRITE_DETECT	TRISAbits.TRISA3    // Input
+#define SD_CS				PORTCbits.RC6	// Chip Select
+#define SD_CS_TRIS			TRISCbits.TRISC6
+#define SD_CD				0 //PORTBbits.RB4	// Card detect
+#define SD_CD_TRIS			TRISBbits.TRISB4
+#define WRITE_DETECT		PORTAbits.RA3	// Write protect
+#define SD_WE				PORTAbits.RA3
 #define SD_WE_TRIS			TRISAbits.TRISA3
 #define SPI_INTERRUPT_FLAG_ASM PIR3, 7
 
 /*******************************************************************/
-/******** Accelerometer definitions and options ********************/
+/******** OLED display selection options    ************************/
 /*******************************************************************/
-#define ACC_CS				LATBbits.LATB4
-#define ACC_CS_TRIS			TRISBbits.TRISB4        // OUTPUT
-#define ACC_INT				PORTBbits.RB2
-#define ACC_INT_TRIS		TRISBbits.TRISB2        // INPUT
+#define	oledWR			LATEbits.LATE1
+#define	oledWR_TRIS		TRISEbits.TRISE1
+#define	oledRD			LATEbits.LATE0
+#define	oledRD_TRIS		TRISEbits.TRISE0
+#define	oledCS			LATEbits.LATE2
+#define	oledCS_TRIS		TRISEbits.TRISE2
+#define	oledRESET		LATDbits.LATD1
+#define	oledRESET_TRIS	TRISDbits.TRISD1
+#define	oledD_C			LATBbits.LATB5
+#define	oledD_C_TRIS	TRISBbits.TRISB5
+// Definitions for reset pin (from oled.h)
+#define RST_TRIS_BIT	TRISDbits.TRISD2
+#define RST_LAT_BIT		LATDbits.LATD2
 
 /*******************************************************************/
-/******** Power Circuit sense pins definitions and options *********/
+/******** Accelerometer definitions and options ********************/
 /*******************************************************************/
-#define USBP_INT_TRIS		TRISDbits.TRISD5   // POK_INT_TRIS = charger "POK"
-#define USBP_INT			PORTDbits.RD5      // POK_INT
-#define PWRGD_INT_TRIS		TRISDbits.TRISD4   // Voltage converter "power good"
-#define PWRGD_INT			PORTDbits.RD4      // PWRGD_INT
+#define ACC_CS			LATCbits.LATC7
+#define ACC_CS_TRIS		TRISCbits.TRISC7
+#define ACC_INT			PORTBbits.RB2
+#define ACC_INT_TRIS	TRISBbits.TRISB2
+
+
+/*******************************************************************/
+/******** USB Power Sense pin definitions and options **************/
+/*******************************************************************/
+#define USBP_INT_TRIS	TRISBbits.TRISB7
+#define USBP_INT		PORTBbits.RB7
+
 
 /*******************************************************************/
 /******** Light Sensor definitions and options *********************/
 /*******************************************************************/
-#define LIGHTCHANNEL		6               // CHS=6 == AN6 == RE1 (PIN26)
-#define LIGHT_PWR			LATEbits.LATE0  // RE0          == RE0 (PIN25)
+#define LIGHTCHANNEL	4
+#define LIGHT_PWR		PORTBbits.RB0
 
 /*******************************************************************/
 /******** remap pins ***********************************************/
 /*******************************************************************/
-#define ul_reg()	{ EECON2 = 0x55; EECON2 = 0xAA; PPSCONbits.IOLOCK = 0; }
-#define l_reg()		{ EECON2 = 0x55; EECON2 = 0xAA; PPSCONbits.IOLOCK = 1; }
-// RP6 as SDO2 (o/p), RP13 as SCK2 (o/p), RP23 as SDI2 (i/P), RP5(B2) as INT1
-#define remap_pins() {ul_reg();RPOR6=9;RPOR13=10;RPINR21=23;RPINR1=5;l_reg();}
+#define ul_reg()	{ EECON2 = 0x55;EECON2 = 0xAA;PPSCONbits.IOLOCK = 0;}
+#define l_reg()		{ EECON2 = 0x55;EECON2 = 0xAA;PPSCONbits.IOLOCK = 1;}
+//  RP6  as SDO2 (o/p), RP13 as SCK2 (o/p), RP23 as SDI2 (i/P),
+#define remap_pins() {ul_reg(); RPOR6=9;RPOR13=10;RPINR21=23;RPINR1=5; l_reg();}
 
-// set pins to input: B2 (acc INT) | D4 (POK) | D5 (PWRGD) | D6 (SPIIN)
-#define set_unused_pins_to_output() {TRISA=0;TRISB=0xb00000100;TRISD=0xb01110000;}
+// make sure all pins are output, except for B7 (USB power) and B5 (acc INT)
+#define set_unused_pins_to_output() {TRISD=0; TRISB=0xb00000101; }
 
-#endif  //HARDWARE_PROFILE_HEDGEHOG_BASIC_418_H
+#endif  //HARDWARE_PROFILE_HEDGEHOG_OLED_513_H
