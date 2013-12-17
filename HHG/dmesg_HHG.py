f6b0b27d6099822ab134bae6d6591b4e7062f944
Hany Abdulsamad <hany.abdulsamad@gmail.com>
2013-12-17 16:27:49 +0100

    dmesg script update

--- HHG/dmesg_HHG.py
+++ HHG/dmesg_HHG.py
@@ -1,26 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 import os
 import time
+import subprocess
 
 
 def getDMESG():
-	commLog = os.popen("dmesg | tail -n 1").read()
-	return commLog
-	#print commLog
+	usbLog = os.popen("dmesg | tail -n 1").read()
+	return usbLog
+
+def getMount():
+	mountLog = os.popen("mount").read()
+	return mountLog
 	
 def compareStatus(currentLogStatus):
 	logStatus = getDMESG()
-	if (logStatus!=currentLogStatus):
+	if 'Attached' in logStatus and logStatus != currentLogStatus:
 		currentLogStatus = logStatus
-		os.system("notify-send \"" + currentLogStatus + "\"")
-	#time.sleep(0.5)
+		subprocess.call(["notify-send", "HedgeHog Device Found"])
+		subprocess.call(["notify-send", "Device is being mounted"])
+		while True:
+			mountStatus = getMount()
+			if 'HEDG' in mountStatus:
+				break
+		subprocess.call(["./download_HHG.py"])
 	return currentLogStatus
 
 def main():
 	currentLogStatus = getDMESG()
 	while (1):
-		currentLogStatus=compareStatus(currentLogStatus)
+		currentLogStatus = compareStatus(currentLogStatus)
+		time.sleep(1)
         
 if __name__ == "__main__":
 	main()
