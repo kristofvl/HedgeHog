3e0b85a499d88e5cbbc674eda4f544c3ec29f5d3
Kristof Van Laerhoven <kryztov@gmail.com>
2014-02-05 16:03:03 +0100

    small additions to the calendar routines

--- HHG/calendar_HHG.py
+++ HHG/calendar_HHG.py
@@ -30,18 +30,40 @@ import sys, time, calendar
 import os, subprocess
 from matplotlib.dates import num2date
 
-def hhg_cal_entry(day_id, month_view):
-	daystr = str(num2date(day_id).year)+' '+str(num2date(day_id).month)  
-	daystr += '-' + str(num2date(day_id).day)
+
+
+## write html header
+def hhg_cal_indexheader(f):
+	f.write('<!DOCTYPE html><html lang=en><meta charset=utf-8>')
+	f.write('<link rel=stylesheet href=st.css>')
+	f.write('<meta property=og:title content="HTML5 HedgeHog Calendar">')
+	
+## write a calendar entry
+def hhg_cal_entry(day_id, month_view, dlpath):
+	daystr = str(num2date(day_id).year)+'-'
+	daystr += str(num2date(day_id).month).zfill(2)
+	daystr += '-' + str(num2date(day_id).day).zfill(2)
 	f.write('<time datetime="'+ daystr + '"')
 	if month_view != num2date(day_id).month:
-		f.write('class="notmonth"')
+		f.write(' class="notmonth"')
 	elif num2date(day_id).weekday()>4:
 		f.write('class="weekend"')
-	f.write('><a href="./'+ str(day_id) +'/p.pdf">' 
+	f.write('><a href="./'+ str(day_id) +'/index.html">' 
 				+ str(num2date(day_id).day) )
 	f.write( '<div class="crop"><img src="./' + str(day_id) + '/p.png' +
 				'"></div></a></time>\n')
+	try:
+		df=open(os.path.join(dlpath,str(day_id),'index.html'),"w")
+		hhg_cal_indexheader(df)
+		df.write('<body><h1>'+daystr+'</h1>')
+		df.write('<p><a href="d.npz">Download the raw data</a>')
+		df.write(' in npz format (numerical python)</p> <center>')
+		df.write('<iframe src="p.pdf" width="1000px" height="700px">')
+		df.write('</iframe></center></body>')
+		df.close()
+	except:
+		print "Date not found"
+	
 
 
 if len(sys.argv) < 2:
@@ -60,21 +82,25 @@ last_day_id = int(sorted(os.walk(dlpath).next()[1])[-1])+1
 month_vw = num2date(first_day_id).month
 
 f=open(os.path.join(dlpath,'index.html'),"w")
-f.write('<!DOCTYPE html><html lang=en><meta charset=utf-8>')
-f.write('<link rel=stylesheet href=st.css>')
-f.write('<meta name=author content="KristofVL">')
-f.write('<meta property=og:title content="HTML5 HedgeHog Calendar">')
+hhg_cal_indexheader(f)
 f.write('<body><section id=calendar>')
-
 f.write('<h1>'+calendar.month_name[month_vw]+' '
 				 +str(num2date(first_day_id).year)+'</h1>')
+for dayname in ('Mon','Tue','Wed','Thu','Fri','Sat','Sun'):
+	f.write('<div class="header">'+dayname+'</div>')
+		
 # fill empty days before day of week:
-for wd in range(num2date(first_day_id).weekday()):
-	f.write('<time datetime=""><a href=#">.')
-	f.write( '<div class="crop"></div></a></time>\n')
+wkday =  num2date(first_day_id).weekday()
+for wd in range(wkday):
+	hhg_cal_entry(first_day_id-wkday+wd, month_vw, dlpath)
+	
 # fill in days with data:
 for day_id in range(first_day_id, last_day_id):
-	hhg_cal_entry(day_id, month_vw)
+	hhg_cal_entry(day_id, month_vw, dlpath)
+	
+# add remaining days in row:
+for rd in range( 7-(last_day_id-first_day_id+wkday)%7 ):
+	hhg_cal_entry(last_day_id+rd, month_vw, dlpath)
 	
 f.write('</section></body>')
 f.close()
