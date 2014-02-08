7b45fd33e2fe62395f0481e1e4a6f77b2de13e27
hanyas <hany.abdulsamad@gmail.com>
2014-02-08 13:29:24 +0100

    retain dmesg code just in case

--- HHG/hhg_io/hhg_import.py
+++ HHG/hhg_io/hhg_import.py
@@ -61,6 +61,18 @@ def hhg_parsedmesg():
 		return True
 	else:
 		return False
+#	log = os.popen("dmesg -T | tail -n 30 | grep HedgeHog").read()
+#	if log:
+#		datestr = log[4:24]
+#		datestr = datestr[:4] + datestr[5:]
+#		logtime = datetime.strptime(datestr, "%b %d %H:%M:%S %Y")
+#		td = datetime.now() - logtime
+#		if (td.seconds < 5):
+#			return True
+#		else:
+#			return False
+#	else:
+#		return False
 		
 ## converts 4 bytes into a matplotlib timestamp
 def hhg_convtime(b1,b2,b3,b4):
