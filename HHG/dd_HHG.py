46d50903275219fa64556a650ad609b9db3622e7
hanyas <hany.abdulsamad@gmail.com>
2014-02-08 12:24:53 +0100

    beautified dd_HHG

--- HHG/dd_HHG.py
+++ HHG/dd_HHG.py
@@ -4,31 +4,34 @@ import sys
 import os
 import subprocess
 from random import randint
+from termcolor import colored
 
 homeDir = os.environ['HOME']
-
 rnd = randint(4,9999)
 
-print('choose a HedgeHog device from the list')
-print('your input should be like /dev/sdd')
+print('Your input should be like /dev/DEVICE')
+print('Choose a HedgeHog device from the list:')
 
 out = subprocess.Popen(['ls', '-l', '/dev/disk/by-label/'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 devices, err = out.communicate()
-print devices
+devices = devices[:-1]
+devices = devices[8:]
+print colored(devices,'green')
 
-target = raw_input('device name: ')
+target = raw_input('Enter your device path:')
 
-print rnd
+print colored('Your passcode is: ' + str(rnd), 'red')
 
-print ('dd is a very dangerous command')
-print ('and could brick your MBR')
-print ('please exit if you are not sure')
-print ('to exit enter 0000')
+print ('dd is a very dangerous tool and could brick your MBR')
+print ('Please exit if you are not sure')
+print ('Press Enter to exit')
 
-code = raw_input('to continue enter the passcode above: ')
+code = raw_input('To continue enter the passcode above: ')
 
-if int(code) == rnd:
-	print('code accepted')
-	subprocess.call(["sudo", "dd", "if=%s/.hhg/dd_img/dd.img" % homeDir, "of=%s" %target])
+if code.isdigit():
+	if int(code) == rnd:
+		print('Code accepted')
+		subprocess.call(["sudo", "dd", "if=%s/.hhg/dd_img/dd.img" % homeDir, "of=%s" %target])
 else: 	
+	print('Script aborted')
 	sys.exit()
