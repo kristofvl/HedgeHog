1612a3e6c6f9ed894dce8d16c55cd55a53ede707
Kristof Van Laerhoven <kryztov@gmail.com>
2014-02-14 16:21:06 +0100

    added month name change

--- HHG/calendar_HHG.py
+++ HHG/calendar_HHG.py
@@ -36,39 +36,66 @@ from matplotlib.dates import num2date
 desc_hhg = {	'names':   ('t',  'd',  'x',  'y',  'z',  'e1', 'e2'), 
 				'formats': ('f8', 'B1', 'B1', 'B1', 'B1', 'u2', 'u2') }
 
-## write html header
-def hhg_day_indexheader():
+## write html header:
+def hhg_day_indexheader(daystr):
 	return ('<!DOCTYPE html><html lang=en><meta charset=utf-8>'+
 		'<link rel=stylesheet href="../st.css">'+
 		'<head><title>HedgeHog Day View</title>'+
-		'<script src="../Chart.js"></script></head>')
+		'<script src="../Chart.js"></script></head>'+
+		'<body><section id="calendar" style="width:1050px;">'+
+		'<h1>'+daystr+'</h1>')
 def hhg_cal_indexheader():
 	return ('<!DOCTYPE html><html lang=en><meta charset=utf-8>'+
 		'<link rel=stylesheet href="st.css">'+
-		'<head><title>HedgeHog Day View</title>'+
-		'<script src="Chart.js"></script></head>')
-	
+		'<head><title>HedgeHog Calendar View</title>'+
+		'<script src="Chart.js"></script>'+
+		'<script src="http://code.jquery.com/jquery-1.11.0.min.js">'+
+		'</script><script>$(document).ready(function(){'+
+		'$("time").mouseover(function(){'+
+		'var mnth = $(this).attr("datetime");$("h1").html(mnth)'+
+		'});});</script></head>')
+		
+## generate html for light dataset variable:
+def hhg_ldata_html(varname, labels, fcolor, scolor, data):
+	return ('var '+varname+'={labels:'+labels+
+		',datasets:[{fillColor:"rgba('+fcolor+')",' + 
+		'strokeColor : "rgba('+scolor+')",data:' + data +'}]};')
+
+## generate html for acc3d dataset variable:	
+def hhg_adata_html(varname, labels, scx, datax, scy, datay, scz, dataz):
+	return ('var '+varname+'={labels:'+labels+
+		',datasets:[{strokeColor : "rgba('+scx+')",data: '+datax+'},'+
+		'{strokeColor : "rgba('+scy+')",data: '+datay+'},'+
+		'{strokeColor : "rgba('+scz+')",data: '+dataz+'}]};')
+
+## generate html for chart variable:
+def hhg_chart_html(varname, idname, charttype, dataname, options):
+	return ('var '+varname+' = new Chart(document.getElementById('+
+		'"'+idname+'").getContext("2d")).'+charttype+'('+dataname+','+
+		'{'+options+'});')
+		
+## return html string to draw the configuration info boxes
 def hhg_conf_html(cnf,smps,rle):
 	g_range = pow(2,1+ord(cnf[12])-48)
 	bw_lookup = [0.1, 5, 10, 25, 50, 100, 200, 400, 800, 1500]
 	md_lookup = ['controller', 'sensor']			
 	pw_lookup = ['normal', 'low-power', 'auto-sleep', 'low/auto']
-	htstr = ('<div id="inf" style="left:850px;top:90px;height:72px;">'+
+	div_preambl = '<div id="inf" style="left:'
+	left_offset = '850px'
+	return (div_preambl+left_offset+';top:90px;height:70px;">'+
 		'<b>HedgeHog Configuration</b>\n'+ 
 		'HedgeHog_ID: '+str(cnf[0:4])+'\nfirmware:    ' + cnf[35:42] +
 		'\nlogging end: 20' +str(ord(cnf[71])) + '-' +
 		str(1+ord(cnf[72])).zfill(2) +'-'+ str(ord(cnf[73])).zfill(2) +
-		'</div><div id="inf" style="left:850px;top:190px;height:84px;">'+
-		'<b>Accelerometer Settings</b>\n' +
+		'</div>'+div_preambl+left_offset+
+		';top:197px;height:84px;"><b>Accelerometer Settings</b>\n' +
 		'acc. range: +/- ' + str(g_range) +'g\nsampled at: ' + 
 		str(bw_lookup[ord(cnf[13])-48]) + 'Hz (' + 
 		str(md_lookup[ord(cnf[14])-48]) + ')\npower mode: ' + 
 		str(pw_lookup[ord(cnf[15])-48]) + '\nRLE delta : ' + str(cnf[20])+
-		'</div><div id="inf" style="left:850px;top:310px;height:50px;">'+
-		'<b>Dataset Properties:</b>\n3d samples : '+str(smps).zfill(9)+
-		'\nRLE samples: '+str(rle).zfill(9)+
-		'</div>')
-	return htstr
+		'</div>'+div_preambl+left_offset+';top:320px;height:48px;">'+
+		'<b>Dataset Properties</b>\n3d samples : '+str(smps).zfill(9)+
+		'\nRLE samples: '+str(rle).zfill(9)+'</div>')
 
 ## write a calendar entry
 def hhg_cal_entry(day_id, month_view, dlpath, f):
@@ -76,7 +103,9 @@ def hhg_cal_entry(day_id, month_view, dlpath, f):
 	daystr = str(num2date(day_id).year)+'-'
 	daystr += str(num2date(day_id).month).zfill(2)
 	daystr += '-' + str(num2date(day_id).day).zfill(2)
-	f.write('<time datetime="'+ daystr + '"')
+	f.write('<time datetime="'+
+		calendar.month_name[num2date(day_id).month]+' '+
+		str(num2date(day_id).year)+ '"')
 	if num2date(day_id).weekday()>4:
 		f.write('class="weekend"')
 	elif month_view != num2date(day_id).month:
@@ -90,7 +119,7 @@ def hhg_cal_entry(day_id, month_view, dlpath, f):
 		cnf = str(out['conf'])
 	except:
 		f.write('</a></time>')
-		print "Data not found"
+		print "Data not found for "+daystr
 		return
 	## open the data and configuration for the day:
 	bins = 14400
@@ -108,37 +137,37 @@ def hhg_cal_entry(day_id, month_view, dlpath, f):
 		'</canvas><canvas style="top:25px;"'+
 		'id="dv_a'+str(day_id)+'" width="160" height="70"></canvas>')
 	f.write('\n<script>')
-	f.write('var dl_'+str(day_id)+'={labels:'+
-		str(['']*(bins/bdiv)).replace(" ","") +
-		',datasets:[{fillColor:"rgba(220,220,0,7)",' + 
-		'strokeColor : "rgba(220,220,220,1)",data:' + 
-		str((day_bin.e1[::bdiv]>>8).tolist()).replace(" ","")+'}]};')
-	f.write('var da_'+str(day_id)+'={labels: ' +
-		str(['']*(bins/bdiv)).replace(" ","")+
-		',datasets:[{strokeColor:"rgba(220,0,0,1)",data:'+
-		str((day_bin.x[::bdiv]).tolist()).replace(" ","")+
-		'},{strokeColor:"rgba(0,170,0,1)",data:'+
-		str((day_bin.y[::bdiv]).tolist()).replace(" ","")+
-		'},{strokeColor:"rgba(0,0,220,1)",data:'+
-		str((day_bin.z[::bdiv]).tolist()).replace(" ","")+'}]};')
-	f.write('var l_'+str(day_id)+'=new Chart(document.getElementById('+
-		'"dv_l'+str(day_id)+'").getContext("2d")).Bar(dl_'+str(day_id)+
-		',{scaleShowLabels:false,scaleShowGridLines:false,'+
-		'scaleFontSize:0,scaleStepWidth:32,animation:false});')
-	f.write('var a_'+str(day_id)+'=new Chart(document.getElementById('+
-		'"dv_a'+str(day_id)+'").getContext("2d")).Line(da_'+str(day_id)+
-		',{scaleShowLabels:false,scaleFontSize:0,'+
-		'scaleLineWidth:0,datasetStrokeWidth:0.5,scaleStepWidth:64});')
+	f.write( hhg_ldata_html('dl_'+str(day_id), 
+					str(['']*(bins/bdiv)).replace(" ",""), 
+					'220,220,0,7', '220,220,220,1', 
+					str((day_bin.e1[::bdiv]>>8).tolist()).replace(" ","")) )
+	f.write( hhg_adata_html('da_'+str(day_id),
+					str(['']*(bins/bdiv)).replace(" ",""),
+					'220,0,0,1', 
+					str((day_bin.x[::bdiv]).tolist()).replace(" ",""),
+					'0,170,0,1',
+					str((day_bin.y[::bdiv]).tolist()).replace(" ",""),
+					'0,0,220,1',
+					str((day_bin.z[::bdiv]).tolist()).replace(" ","") ) )
+	f.write(
+		hhg_chart_html('l_'+str(day_id), 'dv_l'+str(day_id), 'Bar', 
+							'dl_'+str(day_id), 'scaleShowLabels:false,'+
+							'scaleFontSize:0,scaleShowGridLines:false,'+
+							'animation:false,scaleStepWidth:32') )
+	f.write(
+		hhg_chart_html('a_'+str(day_id), 'dv_a'+str(day_id), 'Line', 
+							'da_'+str(day_id), 'scaleShowLabels:false,'+
+							'scaleFontSize:0,scaleLineWidth:0,'+
+							'datasetStrokeWidth:0.5,scaleStepWidth:64') )
 	f.write('</script></time>')
 		
 	## construct the html page for the day-view:
 	try:
 		df=open(os.path.join(dlpath,str(day_id),'index.html'),"w")
 	except:
-		print "Day directory file not found"
+		print "Day directory file not found for "+daystr
 		return
-	df.write(hhg_day_indexheader())
-	df.write('<body><section id="calendar"><h1>'+daystr+'</h1>')
+	df.write(hhg_day_indexheader(daystr))
 	df.write('<p>Detailed 24h view for '+daystr+' with HedgeHog'+
 		' sensor #'+ cnf[0:4]+
 		'. Raw data download: <a href="d.npz">here</a> (npz format, '+
@@ -150,24 +179,19 @@ def hhg_cal_entry(day_id, month_view, dlpath, f):
 	df.write(hhg_conf_html(cnf,sum(dta.view(np.recarray).d),len(dta)))
 	lbl = [' ']*bins;lbl[0]='00';lbl[int(bins/4)]='06';lbl[bins>>1]='12';
 	lbl[int(3*bins/4)]='18';lbl[bins-1]='00';
-	df.write('<script>var data_light={'+
-		'labels:'+str(['']*int(bins/bdiv)).replace(" ","")+
-		',datasets:[{fillColor : "rgba(220,220,0,7)",'+
-		'strokeColor:"rgba(220,220,220,1)",data:'+
-		str((day_bin.e1[::bdiv]>>8).tolist()).replace(" ","")+
-		'}]};var data_acc3d = {labels: '+str(lbl).replace(" ","")+
-		',datasets:[{strokeColor : "rgba(220,0,0,1)",data: '+
-		str((day_bin.x).tolist()).replace(" ","")+'},'+
-		'{strokeColor : "rgba(0,170,0,1)",data: '+
-		str((day_bin.y).tolist()).replace(" ","")+'},'+
-		'{strokeColor : "rgba(0,0,220,1)",data: '+
-		str((day_bin.z).tolist()).replace(" ","")+'}]};')
-	df.write('var light = new Chart(document.getElementById('+
-		'"day_view_light").getContext("2d")).Bar(data_light,'+
-		'{scaleStepWidth:32});')		
-	df.write('var acc3d = new Chart(document.getElementById('+
-		'"day_view_acc3d").getContext("2d")).Line(data_acc3d,'+
-		'{scaleSteps:8,scaleStepWidth:32});')
+	df.write('<script>')
+	df.write( hhg_ldata_html('data_light', 
+					str(['']*int(bins/bdiv)).replace(" ",""), 
+					'220,220,0,7', '220,220,220,1', 
+					str((day_bin.e1[::bdiv]>>8).tolist()).replace(" ","")) )
+	df.write( hhg_adata_html('data_acc3d', str(lbl).replace(" ",""),
+					'220,0,0,1', str((day_bin.x).tolist()).replace(" ",""),
+					'0,170,0,1', str((day_bin.y).tolist()).replace(" ",""),
+					'0,0,220,1', str((day_bin.z).tolist()).replace(" ","")) )
+	df.write( hhg_chart_html('light', 'day_view_light', 'Bar', 
+								'data_light', 'scaleStepWidth:32') )
+	df.write( hhg_chart_html('acc3d', 'day_view_acc3d', 'Line', 
+								'data_acc3d', 'scaleSteps:8,scaleStepWidth:32'))
 	df.write('</script></section></body></html>')
 	df.close()
 
@@ -192,7 +216,7 @@ f=open(os.path.join(dlpath,'index.html'),"w")
 f.write(hhg_cal_indexheader())
 f.write('<body><section id=calendar>')
 f.write('<h1>'+calendar.month_name[month_vw]+' '
-				 +str(num2date(first_day_id).year)+'</h1>')
+				 +str(num2date(first_day_id).year)+'</h1>')	
 for dayname in ('Mon','Tue','Wed','Thu','Fri','Sat','Sun'):
 	f.write('<div class="header">'+dayname+'</div>')
 f.write('<div id="days"><div id="scrollview">')
@@ -209,8 +233,9 @@ for day_id in range(first_day_id, last_day_id):
 # add remaining days in row:
 for rd in range( 7-(last_day_id-first_day_id+wkday)%7 ):
 	hhg_cal_entry(last_day_id+rd, month_vw, dlpath, f)
-	
-f.write('</div></div></section></body>')
+
+f.write('</div></div>')
+f.write('</section></body>')
 f.close()
 
 subprocess.call(["firefox", "%s/index.html"%dlpath])
