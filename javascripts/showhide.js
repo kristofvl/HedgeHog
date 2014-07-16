027eb7d2e67806ea80864bb3a1df916880e086e5
Kristof Van Laerhoven <kvl@s2935.dyn.hrz.tu-darmstadt.de>
2014-07-16 16:21:07 +0200

    update

--- javascripts/showhide.js
+++ javascripts/showhide.js
@@ -1,12 +1,13 @@
 function showHide(shID) {
    if (document.getElementById(shID)) {
-      if (document.getElementById(shID+'-show').style.display != 'none') {
-         document.getElementById(shID+'-show').style.display = 'none';
+      if (document.getElementById(shID).style.display != 'block') {
          document.getElementById(shID).style.display = 'block';
+	  document.getElementById(shID+'-show').className = "hideLink";
+	  console.log(document.getElementById(shID).className);
       }
       else {
-         document.getElementById(shID+'-show').style.display = 'inline';
          document.getElementById(shID).style.display = 'none';
+	  document.getElementById(shID+'-show').className = "showLink";
       }
    }
 }
