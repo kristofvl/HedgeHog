d7cc32b40317bd66b318b07bd726fe72fc4d41a2
hwlab <hwlab@ariel.ess.informatik.tu-darmstadt.de>
2014-07-20 14:23:41 +0200

    CAREFUL: dd script formats any card now

--- HHG/dd_HHG.py
+++ HHG/dd_HHG.py
@@ -9,16 +9,17 @@ homeDir = os.environ['HOME']
 deviceList = [];
 
 if os.path.exists("/dev/disk/by-label"):
-	deviceStr = os.popen("ls /dev/disk/by-label/ | grep HEDGE").read()
+	deviceStr = os.popen("ls /dev/disk/by-label/").read()
 else: 
-	print ('No HedgeHog device found')
+	print ('No SD Card/USB Device found')
 	print ('Script aborted')
 	sys.exit()
 	
 if not deviceStr:
-	print ('No HedgeHog device found')
+	print ('No SD Card/USB Device found')
 	print ('Script aborted')
 	sys.exit()	 
+	
 else: 
 	indexNL = [i for i, ltr in enumerate(deviceStr) if ltr == '\n']
 	deviceList.insert(0,deviceStr[0:indexNL[0]])
@@ -26,8 +27,9 @@ else:
 		deviceList.insert(i,deviceStr[indexNL[i-1]+1:indexNL[i]])
 	for i in xrange(0,len(deviceList)):
 		print (str(i)+ ').' + deviceList[i])
-		
-target = raw_input('Choose the number of a HedgeHog device from the list above:')
+
+print ('Choose the number of a HedgeHog device from the list above:')
+target = raw_input('Make sure the device you are choosing is actually a HedgeHog device:')
 
 if int(target) < len(deviceList):
 		subprocess.call(["sudo", "dd", "if=%s/.hhg/dd_img/dd.img" % homeDir, "of=/dev/disk/by-label/%s" %str(deviceList[int(target)])])
