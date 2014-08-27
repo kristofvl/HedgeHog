16f5a8e34a4f788c425e1b33899ccb8d4d8347d3
Kristof Van Laerhoven <kryztov@gmail.com>
2014-08-27 10:43:46 +0200

    added basis for calbrowsing again

--- HHG/hhg_web/cal.js
+++ HHG/hhg_web/cal.js
@@ -1,53 +1,47 @@
 var monthns = ["January", "February", "March", "April","May","June",
 	"July","August","September","October","November","December"];
-	
+
+var start_day = -1
+
 // fills the month table with numbers
 function fill_table(month,year)
 { 
   day=1
-  start_day=(new Date(year,month,1)).getDay()
+  if (start_day==-1) 
+	start_day=(new Date(year,month,1)).getDay()
   stop_day=(new Date(year,month+1,0)).getDate()
-  console.log(start_day)
-  console.log(stop_day)
-  document.write("<td><table border=1 cellspacing=0><tr>")
-  document.write("<td colspan=7 align=center><b>"+monthns[month]+"</b>")
-  document.write("<tr>")
-  if (start_day==0) start_day = 7;
+  document.write("<td><table id='calm' cellspacing=0><tr>")
+  document.write("<td colspan=7><b>"+monthns[month]+"</b><tr>")
+  if (start_day==8) 
+	start_day=1
   for (var i=1;i<start_day;i++){
         document.write("<td></td>")
   }
   for (var i=start_day;i<8;i++){
-        document.write("<td align=center>"+day+"</td>")
+        document.write("<td>"+day+"</td>")
         day++
   }
-  document.write("<tr>")
+  document.write("</tr>")
   while (day <= stop_day) {
      for (var i=1;i<=7 && day<=stop_day;i++){
-         document.write("<td align=center>"+day+"</td>")
+         document.write("<td>"+day+"</td>")
          day++
      }
      document.write("</tr><tr>")
   }
+  if (start_day-1+stop_day<36)
+	document.write("</tr><tr><td></td>")
+  start_day = i
   document.write("</tr></table></td>")
-  return i
 }
 
 function addMonthBrowser(year) {
-	document.write('<div id="ycal" style="display:none;position:absolute;left:600px;z-index:2;font-size:10px;"><table border=1 cellspacing=0 callpadding=0 bgcolor="#fff"><tr>')
-	fill_table("January",31,year)
-	fill_table("February",29,year)
-	fill_table("March",31,year)
-	fill_table("April",30,year)
-	document.write("</tr><tr>")
-	fill_table("May",31,year)
-	fill_table("June",30,year)
-	fill_table("July",31,year)
-	fill_table("August",31,year)
-	document.write("</tr><tr>")
-	fill_table("September",30,year)
-	fill_table("October",31,year)
-	fill_table("November",30,year)
-	fill_table("December",31,year)
+	document.write('<div id="ycal" style="display:none;"><table id="calt"><tr>')
+	for (var i=0; i<12; i++) {
+		fill_table(i,year)
+		if ((i+1)%4==0)
+			document.write("</tr><tr>")
+	}
 	document.write("</tr></table>")
 	document.write("</div>")
 }
